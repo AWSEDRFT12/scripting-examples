133418f72dd7b0c8cb53e32b2e5e067a003a3336
AWSEDRFT12 <anthonyfrancisco503@riversideunified.org>
2023-10-04 11:36:18 -0700

    Update Variables.cs

--- Scripting Fundamentals/Assets/Scripts/Variables.cs
+++ Scripting Fundamentals/Assets/Scripts/Variables.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
+    // Declaring Variables
     // Public Member/Gobal Variable 
     public float MoveSpeed = 10f;
     public int PlayerScore = 0;
     public string PlayerName = "Mr scott";
     public bool IsPlayerActive = true;
+
+    // Private Member/Gobal Variables 
+    private float _maxspeed = 100f;
+    private int _totalScore = 0;
+    private string _userName = "Scott621";
+    private bool isGameOver = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +26,13 @@ public class Variables : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Declaring local varibles 
+        float horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void UpdateScore(int amount)
+    {
+        // using a local variable to update the total score
+        _totalScore += amount;
     }
 }
