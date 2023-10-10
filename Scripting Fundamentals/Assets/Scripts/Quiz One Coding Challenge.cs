18137e4d941dc5318f8382b465e32261dea529cb
AWSEDRFT12 <anthonyfrancisco503@riversideunified.org>
2023-10-10 11:11:54 -0700

    Updating Files

---
+++ Scripting Fundamentals/Assets/Scripts/Quiz One Coding Challenge.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizOneCodingChallenge : MonoBehaviour
+{
+    // Declaring Variables 
+    // Public Member/Gobal Variable
+    
+    public int Score = 0;
+    public string PlayerName = "Anthony";
+    public bool ISplayerActive = true;
+
+    // Private Member/Gobal Varible 
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Method Increase score 
+    private void UpdateScore(int amount)
+    {
+        
+    }
+}
